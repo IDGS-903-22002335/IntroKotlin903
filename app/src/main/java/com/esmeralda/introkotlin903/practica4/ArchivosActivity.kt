bddb0920524e3454a82a1bd497362171f8cb94cb
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-05-22 19:20:43 -0600

    archivo de texto

--- app/src/main/java/com/esmeralda/introkotlin903/practica4/ArchivosActivity.kt
+++ app/src/main/java/com/esmeralda/introkotlin903/practica4/ArchivosActivity.kt
@@ -42,7 +42,7 @@ class ArchivosActivity : AppCompatActivity() {
                     lines.joinToString("\n")
                 }
                 outPut.text=contenido
-            }catch (e:FileNotFoundException){
+            }catch (e:FileNotFoundException){gir
                 outPut.text="Archivo no encontrado"
             }catch (e: Exception){
                 e.printStackTrace()
